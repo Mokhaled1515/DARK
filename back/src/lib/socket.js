7f25287fac01c49a2e5ce5af89af0e6cb3857224
Mohamed <mookhaleddd@gmail.com>
2026-09-11 23:25:18 +0300

    Fix Socket.IO CORS

--- back/src/lib/socket.js
+++ back/src/lib/socket.js
@@ -7,15 +7,46 @@ import Message from "../models/Messege.js"; // 👈 1. أضفنا استدعاء
 const app = express();
 const server = http.createServer(app);
 
+// const allowedOrigins = [
+//   // process.env.FRONT_END_URL || "https://DARKii.vercel.app",
+//   // "http://localhost:5173",
+//   // "http://localhost:3000",
+//   "https://dark-mo-01.vercel.app",
+//   "http://localhost:5173",
+//   "http://localhost:3000",
+// ];
+// console.log("Allowed CORS origins:", allowedOrigins);
+
 const allowedOrigins = [
-  process.env.FRONT_END_URL || "https://DARKii.vercel.app",
+  process.env.FRONT_END_URL,
+  "https://dark-mo-01.vercel.app",
   "http://localhost:5173",
   "http://localhost:3000",
-];
+].filter(Boolean);
+
+console.log("Allowed origins:", allowedOrigins);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
 
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      console.log("Incoming origin:", origin);
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("❌ CORS rejected:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
